3d6e057587df85519cde7eaeea5dae0d528029c1
ayepw <ayepw@LAPTOP-NMCRE0FC>
2024-09-16 22:05:43 +0800

    Add project files.

---
+++ APPDontnetTrainingBatch4.MiddlewareApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APPDontnetTrainingBatch4.MiddlewareApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ APPDotNetTrainingBatch4.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APPDotNetTrainingBatch4.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
